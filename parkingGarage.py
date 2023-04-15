22f448b8f79e8484e15542cd71dac640880959c3
Chris Eakins <ceakins92@gmail.com>
2023-04-14 18:17:04 -0600

    Updated with changes made during Live Share

--- parkingGarage.py
+++ parkingGarage.py
@@ -7,32 +7,34 @@ class parking_gargage():
                      1016, 1017, 1018, 1019, 1020, 1021, 1022]
     parking_spaces = [5, 7, 10, 21, 22, 46, 56, 72, 73, 74, 76, 81]
     current_ticket = {}
-    taken_spaces = {}
+
+   # tickets_avail = 100
 
     def main_menu(self):
-        pass
+
         # ARRIVAL / TAKING TICKET
         # user arrives, gets prompt to take ticket, pay ticket, or cancel
         parking = True
-        while True:
-            print('***** Welcome to the RC Parking Garage! *****\n')
-            print(
-                f'* There are currently {len(self.parking_spaces)} available *\n')
-            response = input(
-                'Select: Take Ticket | Pay Ticket | Cancel ?\n').lower()
+        while parking:
 
-            if response == 'take' or 'take ticket':
+            print('\n*********************************************')
+            print('*     Welcome to the RC Parking Garage!     *')
+            print(
+                f'* There are currently {len(self.parking_spaces)} spaces available   *')
+            print('*********************************************\n')
+            response = input(
+                'Select: Take Ticket | Pay Ticket | Cancel ?\n')
+
+            if response.lower() == 'take':
                 self.take_ticket()
 
-            elif response == 'pay' or 'pay ticket':
+            if response.lower() == 'pay':
                 self.pay_for_parking()
 
-            elif response == 'cancel' or 'quit':
-                self.show()
-                parking = False
-
-            else:
-                print('Please choose from the list of options:\n')
+            if response.lower() == 'cancel':
+                # parking = False
+                break
+            #    print('Please choose from the list of options:\n')
 
         # PAYING FOR TICKET
         # user gets prompted to pay for ticket (maybe by ticket number?)
@@ -45,7 +47,20 @@ class parking_gargage():
         # ----------- return to main prompt
 
     def take_ticket(self):
-        pass
+        # self.tickets_avail.pop()
+        if len(self.parking_spaces) <= 11:
+            print('\nSorry, one ticket per customer please')
+            self.pay_for_parking()
+        else:
+            self.current_ticket = {
+                self.tickets_avail.pop(): self.parking_spaces.pop(),
+            }
+            print(
+                f'\nYour ticket and parking space number are: {self.current_ticket}.')
+            # number is: {self.current_ticket.key()}. You have been assigned parking spot number: {self.current_ticket.value()}\n')
+            print(self.current_ticket)
+        # print(self.current_ticket)
+        # self.main_menu()
         # - This should decrease the amount of tickets_avail available by 1
         # --- (maybe add a timer to calculate total that stops when they leave)
         # - This should decrease the amount of parking_spaces available by 1
@@ -69,3 +84,7 @@ class parking_gargage():
         # - Update parking_spaces list to increase by 1 (meaning add to the parkingSpaces list)
         # - Update tickets_avail list to increase by 1 (meaning add to the tickets list)
         # - Return to main prompt
+
+
+user1 = parking_gargage()
+user1.main_menu()
