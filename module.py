adf81eb52aae2b739da8c6f3594eab074c3448b1
Chris Eakins <ceakins92@gmail.com>
2023-04-15 11:08:12 -0600

    Updated output formatting, fixed payment repeating output error

--- module.py
+++ module.py
@@ -8,7 +8,8 @@ class parking_gargage():
     def main_menu(self):
 
         while True:
-
+            print('\n')
+            print('\n')
             print('\n*********************************************')
             print('*     Welcome to the RC Parking Garage!     *')
             print(
@@ -23,43 +24,52 @@ class parking_gargage():
                 self.pay_for_parking()
 
             if response.lower() == 'cancel':
-                # parking = False
                 break
 
     def take_ticket(self):
 
         self.current_ticket.update(
             {self.tickets_avail.pop(): self.parking_spaces.pop()})
+        print('\n')
+        print('\n')
+        print('\n*********************************************')
         print(
-            f'\nYour ticket and parking space number are: {list(self.current_ticket.items())[-1]}.')
-
-        print(self.current_ticket)
+            f'Your ticket and parking space \nnumber are: {list(self.current_ticket.items())[-1]}.')
 
     def pay_for_parking(self):
 
-        print('Thank You for parking with us')
-
-        for k, v in self.current_ticket.items():
-            self.temporary_list.append(k)
-
-        for k, v in self.current_ticket.items():
-            self.temporary_list.append(v)
-        if self.current_ticket:
-            self.current_ticket.popitem()
-        else:
-            self.leave_garage()
-
-        print(self.current_ticket)
-
         if len(self.parking_spaces) >= 12:
-            print('you have already paid!')
+            print('\n')
+            print('\n')
+            print('\n*********************************************')
+            print('There is no ticket to pay!')
+
         else:
+
+            for k, v in self.current_ticket.items():
+                self.temporary_list.append(k)
+
+            for k, v in self.current_ticket.items():
+                self.temporary_list.append(v)
+
+            if self.current_ticket:
+                self.current_ticket.popitem()
+            else:
+                print('There is no ticket to pay!')
+                self.main_menu()
+
+        # if len(self.parking_spaces) >= 12:
+        #    print('\n*********************************************')
+        #    print('There is no ticket to pay!')
+        # else:
             self.parking_spaces.append(self.temporary_list.pop())
             self.tickets_avail.append(self.temporary_list.pop())
-
-        print('Your ticket has been paid, you have 15 min to leave!')
-
-        self.leave_garage()
+            print('\n')
+            print('\n')
+            print('\n*********************************************')
+            print('Your ticket has been paid,\nyou have 15 min to leave!\n')
+            print('Thank You for parking with us')
+            self.leave_garage()
 
     def leave_garage(self):
-        print('Thank you have a nice day')
+        print('Have a nice day! :\)')
